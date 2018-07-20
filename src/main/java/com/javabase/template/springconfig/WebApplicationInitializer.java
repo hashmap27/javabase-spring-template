5f750830934277995a0ff8d7eaabb6c331ab80c7
SungHyun Cheong <hashmap27@gmail.com>
2018-07-20 19:02:54 +0900

    한글깨짐 방지 인코딩필터 추가.

--- src/main/java/com/javabase/template/springconfig/WebApplicationInitializer.java
+++ src/main/java/com/javabase/template/springconfig/WebApplicationInitializer.java
@@ -1,5 +1,12 @@
 package com.javabase.template.springconfig;
 
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.joda.time.DateTime;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.javabase.template.springconfig.appservlet.WebMvcConfig;
@@ -11,6 +18,18 @@ import com.javabase.template.springconfig.root.RootConfig;
  * @author Choeng SungHyun <hashmap27@gmail.com>
  */
 public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        //로그 출력
+        StringBuilder sb = new StringBuilder();
+        sb.append("\r\n=====================================");
+        sb.append("\r\n- JavaBase Spring Configuration Template Start Up");
+        sb.append("\r\n- " + DateTime.now());
+        sb.append("\r\n=====================================");
+        logger.warn(sb.toString());
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -29,6 +48,23 @@ public class WebApplicationInitializer extends AbstractAnnotationConfigDispatche
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        //NOTE: Dispatcher Servlet 필터 등록
+        return new Filter[] {
+                new ShallowEtagHeaderFilter(),
+                getCharacterEncodingFilter()
+        };
+    }
+
+    //한글깨짐 방지 - UTF-8 인코딩
+    protected CharacterEncodingFilter getCharacterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
     }
 
 }
