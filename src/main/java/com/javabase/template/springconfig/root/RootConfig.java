e5235093f6d6a889e2c70bc57dcdf47a5674f9e4
SungHyun Cheong <hashmap27@gmail.com>
2018-07-20 15:20:43 +0900

    RootConfig에 componentScan 설정 추가

--- src/main/java/com/javabase/template/springconfig/root/RootConfig.java
+++ src/main/java/com/javabase/template/springconfig/root/RootConfig.java
@@ -1,6 +1,12 @@
 package com.javabase.template.springconfig.root;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * root-context.xml의 역할을 대신하거나 보충하는 클래스
@@ -8,6 +14,19 @@ import org.springframework.context.annotation.Configuration;
  * @author Cheong SungHyun <hashmap27@gmail.com>
  */
 @Configuration
+@ComponentScan(
+        basePackages = "com.javabase",
+        useDefaultFilters = false,
+        excludeFilters = {
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = Controller.class),
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = RestController.class)
+        },
+        includeFilters = {
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = Configuration.class),
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = Service.class),
+                @ComponentScan.Filter(type=FilterType.ANNOTATION, classes = Repository.class)
+        }
+)
 public class RootConfig {
 
 }
