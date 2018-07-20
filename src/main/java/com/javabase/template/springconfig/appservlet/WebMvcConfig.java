90cfed1fec2f100e15abbf3001e326194c7ad1d6
SungHyun Cheong <hashmap27@gmail.com>
2018-07-20 20:35:40 +0900

    cors 설정 추가. 주석 추가. 소스 정리.

--- src/main/java/com/javabase/template/springconfig/appservlet/WebMvcConfig.java
+++ src/main/java/com/javabase/template/springconfig/appservlet/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.javabase.template.springconfig.appservlet;
 
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,7 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -46,6 +49,27 @@ import com.javabase.template.framework.converter.StringToJodaLocalDateTimeConver
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(false)                        //HttpReqeust Header의 Accept 무시여부
+                    .favorPathExtension(true)                       //프로퍼티 값을 보고 URL의 확장자에서 리턴 포맷을 결정 여부
+                    .ignoreUnknownPathExtensions(true)              //모든 미디어 유형으로 해결할 수 없는 경로 확장자를 가진 요청을 무시할지 여부
+                    .favorParameter(true)                           //URL호출 시 특정 파라미터로 리턴포맷 전달 허용 여부
+                    .mediaType("xml", MediaType.APPLICATION_XML)
+                    .mediaType("json", MediaType.APPLICATION_JSON)
+                    .defaultContentType(MediaType.TEXT_HTML);
+    }
+
+    @Override
+    /** Spring Formatter */
+    public void addFormatters(FormatterRegistry registry) {
+        //Deserialize시에 사용할 Formatter
+        //String To Joda Date Converting added.
+        //Serialize시에는 다른 설정이 필요함.
+        registry.addConverter(new StringToJodaDateTimeConverter());
+        registry.addConverter(new StringToJodaLocalDateTimeConverter());
+    }
+
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //NOTE: 입맛대로 수정
         registry.addResourceHandler("/favicon.ico").addResourceLocations("/").setCacheControl(CacheControl.maxAge(1, TimeUnit.HOURS).mustRevalidate());
@@ -58,23 +82,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.ignoreAcceptHeader(false)                        //HttpReqeust Header의 Accept 무시여부
-                    .favorPathExtension(true)                       //프로퍼티 값을 보고 URL의 확장자에서 리턴 포맷을 결정 여부
-                    .ignoreUnknownPathExtensions(true)              //모든 미디어 유형으로 해결할 수 없는 경로 확장자를 가진 요청을 무시할지 여부
-                    .favorParameter(true)                           //URL호출 시 특정 파라미터로 리턴포맷 전달 허용 여부
-                    .mediaType("xml", MediaType.APPLICATION_XML)
-                    .mediaType("json", MediaType.APPLICATION_JSON)
-                    .defaultContentType(MediaType.TEXT_HTML);
+    /**
+     * Cross-Origin Resource Sharing
+     * 웹페이지에서 ajax를 사용할 때 다른 도메인의 서버 리소스(JSON 등)에 접근하기 위한 메커니즘.
+     * 자세한 내용은 W3C 홈페이지에서 확인 - http://www.w3.org/TR/cors/
+     */
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  //CORS기능 사용 경로 지정(ROOT)
+                .allowedOrigins("*")    //접근 허용 Origin(도메인) 지정. 기본적으로 무제한이라는 의미로 '*'이 적용
+                .allowedMethods("GET", "POST", "PUT", "DELETE")     //접근을 허용할 HTTP 메소드 지정. default: GET, HEAD, POST가 하용.
+                .allowCredentials(false)    //인증정보(쿠키 등)를 취급할지 여부 결정 default: true
+                .maxAge(3600);   //클라이언트가 요청에 대한 응답을 캐시할 시간(초 단위)을 지정. default: 1800(30분
     }
 
     @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToJodaDateTimeConverter());
-        registry.addConverter(new StringToJodaLocalDateTimeConverter());
-    }
-
-    @Override
+    /** Spring에서 사용할 ViewResolver 설정 */
     public void configureViewResolvers(ViewResolverRegistry registry) {
         MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
         jsonView.setPrettyPrint(true);
@@ -82,6 +104,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         xmlView.setPrettyPrint(true);
         registry.enableContentNegotiation(jsonView, xmlView);
         registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    /** 파일업로드 */
+    @Bean(name="multipartResolver") //Spring default bean name
+    public CommonsMultipartResolver commonsMultipartFile() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setDefaultEncoding("utf-8");
+        //NOTE: 파일 업로드 제한 가능.
+        //resolver.setMaxUploadSize(-1);    //파일 하나의 최대 바이트 수를 지정. default: -1(제한 없음)
+        //if(!StringUtils.isEmpty(appConfig.getUploadTempDir()) {   //업로드 파일 임시 저장 경로
+        //    FileSystemResource resource = new FileSystemResource(appConfig.getUploadTempDir());
+        //    resolver.setUploadTempDir(resource);
+        //}
+        return resolver;
     }
 
 }
