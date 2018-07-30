0831e4dde8659a05c52c38d5213fe8303e6f77a8
SungHyun Cheong <hashmap27@gmail.com>
2018-07-30 15:13:18 +0900

    Component -> Configuration으로 수정.

--- src/main/java/com/javabase/template/config/ProfilesManager.java
+++ src/main/java/com/javabase/template/config/ProfilesManager.java
@@ -9,15 +9,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 /**
  * Spring Profile Manager
  *
  * @author Cheong SungHyun <hashmap27@gmail.com>
  */
-@Component
+@Configuration
 public class ProfilesManager implements InitializingBean {
     Logger logger = LoggerFactory.getLogger(ProfilesManager.class);
 
